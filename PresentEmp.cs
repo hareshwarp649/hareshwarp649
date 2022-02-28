1b268a993996745c82f5c29b1202ab2035b33fa9
Harry <hareshwarp649>
2022-02-28 07:20:04 +0530

    Check employee is present or absent

---
+++ PresentEmp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConce
+{
+    public class PresentEmp
+    {
+
+
+        public int IS_FULL_TIME;
+
+        public PresentEmp(int d)
+        {
+            IS_FULL_TIME = d;
+        }
+        public void CheckEmp()
+        {
+            IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+         
+                Console.WriteLine("Employee is Absent");
+            
+        }
+
+    }
+}
