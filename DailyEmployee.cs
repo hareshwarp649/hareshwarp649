d0d733475ee739a3e9be78b0ce053d0aa62e15c9
Harry <hareshwarp649>
2022-02-28 07:25:31 +0530

    Calculate daily  employee wage and part time

---
+++ DailyEmployee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConce
+{
+   public class DailyEmployee
+    {
+
+        public int IS_FULL_TIME = 1;
+        public int IS_PART_TIME = 2;
+
+        public DailyEmployee(int a, int d)
+        {
+            IS_FULL_TIME = d;  
+            IS_PART_TIME = a;
+        }
+
+       
+        public void EmployeeDaily()
+        {
+
+           int EMP_RATE_PER_HOUR = 20; 
+
+           int empHrs = 0;
+           int empWage = 0;
+
+           Random random = new Random();
+
+           int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+           {
+            empHrs = 0;
+           }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage :" + empWage);
+
+        }
+    }
+}
