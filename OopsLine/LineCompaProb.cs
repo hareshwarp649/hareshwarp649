8d2e9260b7ddfcd5fceddc98661a93287a23e1a9
Harry <hareshwarp649>
2022-03-01 12:02:51 +0530

    Line comparison priblem using Oops

---
+++ OopsLine/LineCompaProb.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWagePro
+{
+     class LineCompaProb
+    {
+        public  double ComparisonProb()
+        {
+            int x1, x2, x3,x4, y1, y2, y3, y4;
+            double line1,line2;
+            
+           
+            Console.WriteLine("Enter the values of x1 and y1 coordinates of first point");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the values of x2 and y2 coordinates of second point");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+          
+           line1= Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+            Console.WriteLine("Enter the values of x3 and y3 coordinates of third point");
+            x3 = Convert.ToInt32(Console.ReadLine());
+            y3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the values of x4 and y4 coordinates of third point");
+            x4 = Convert.ToInt32(Console.ReadLine());
+            y4 = Convert.ToInt32(Console.ReadLine());
+           
+            line2=Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y2 - y1), 2));
+
+            Console.WriteLine("Lenght of line1 is:" + line1);
+            Console.WriteLine("Lenght of line2 is:" + line2);
+
+            if (line1 > line2)
+              {
+               
+                    Console.WriteLine("{0} is greater than {1}", line1, line2);
+               }
+            
+            else if (line2 > line1)
+            {
+                Console.WriteLine("{1} is less than {0}", line2, line1);
+            }
+            else
+            {
+                Console.WriteLine("{0} and {1} are equal", line1, line2);
+            }
+            Console.ReadLine();
+            return line1;
+           
+        }
+    }
+}
+      
---
+++ OopsLine/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWagePro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to Employee Wage Program");
+            //EmpCheck.EmpPresentOrNot();
+            // CalculateEmpWage.CalEmpWage();
+            // SwitchCalEmpWage.CalEmpWage();
+            //  HourseCalEmpWage.CalHourse(); 
+            // SnakeLadderProb.SimuLadder();
+            //    SnakeLadderProb gm = new SnakeLadderProb();
+            //  gm.SimuLadder()
+            //   LineCompaProb.ComparisonProb();
+            // DailyEmpWage.CalcuDaily();
+            // SnakePlayer.PlayerSnake();
+            // SnakePlayer player1 = new SnakePlayer();
+            //SnakePlayer player2 = new SnakePlayer();
+
+            LineCompaProb length1 = new LineCompaProb();
+            LineCompaProb length2 = new LineCompaProb();
+
+            double line1 = length1.ComparisonProb();
+
+            double line2 = length1.ComparisonProb();
+
+
+        }
+    }
+}
